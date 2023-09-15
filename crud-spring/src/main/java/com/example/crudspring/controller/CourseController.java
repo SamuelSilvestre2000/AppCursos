705259dd7606138f6955347313b53d4e5d9d00b3
SamuelSilvestre2000 <samuelsilvestre2000@gmail.com>
2023-09-15 11:49:26 -0300

    Componente de Formulário e Roteamento para criar cursos

--- crud-spring/src/main/java/com/example/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/example/crudspring/controller/CourseController.java
@@ -1,1 +1,30 @@
+package com.example.crudspring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.crudspring.model.Course;
+import com.example.crudspring.repository.CourseRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController{
+
+
+    private CourseRepository courseRepository;
+
+    
+    public CourseController(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+
+    @GetMapping
+    public List<Course> list(){
+        return courseRepository.findAll();
+    }
+}
